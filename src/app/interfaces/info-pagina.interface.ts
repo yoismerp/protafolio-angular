37e5c001310a54aa1af1415b4e99cddedcb2565e
yoismer perez <xfxrickjames@gmail.com>
2020-08-29 00:57:31 -0400

    listo para preproduccion

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,14 @@
+export interface InfoPagina {
+
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twiter?: string;
+    instagram?: string;
+    tublr?: string;
+    equipo_trabajo?: any [];
+
+}
+
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
+
+    this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
+
+      this.cargada = true;
+
+      this.info = resp;
+
+    });
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-2b3dd.firebaseio.com/equipo.json').subscribe((resp: any[]) => {
+
+      this.cargada = true;
+
+      this.equipo = resp;
+
+    });
+  }
+}
